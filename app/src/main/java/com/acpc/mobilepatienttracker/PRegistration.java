94f8dab81a17a361e34b6094624cc7316c5a5898
kimeeramoodley <1835687@students.wits.ac.za>
2021-04-06 20:43:46 +0200

    Initial commit (patient form)

--- app/src/main/java/com/acpc/mobilepatienttracker/PRegistration.java
+++ app/src/main/java/com/acpc/mobilepatienttracker/PRegistration.java
@@ -3,6 +3,7 @@ package com.acpc.mobilepatienttracker;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.util.Patterns;
 import android.view.View;
@@ -97,17 +98,25 @@ public class PRegistration extends AppCompatActivity implements View.OnClickList
                                     .setValue(user).addOnCompleteListener(new OnCompleteListener<Void>() { //To check if data has been inserted into database
                                 @Override
                                 public void onComplete(@NonNull Task<Void> task) {
+
                                     if(task.isSuccessful()){
                                         Toast.makeText(PRegistration.this, "User Has Successfully Been Registered", Toast.LENGTH_LONG).show(); //shows message to tell user registration has been successful
 
 
                                         ///////Redirect to login now !
-                                    }
+
+                                        ///////OR redirect to patient form
+
+                                                Intent start = new Intent(PRegistration.this,PatientForm.class); //moving from main screen to reg screen when clicking register button on main screen
+                                                startActivity(start);
+
+
+                                    }// endif
                                     else{
                                         Toast.makeText(PRegistration.this, "Registration Unsuccessful, Try Again", Toast.LENGTH_LONG).show(); //unsuccessful registration message
 
                                     }
-                                }
+                                }// endof onComplete
                             });
 
                         }
