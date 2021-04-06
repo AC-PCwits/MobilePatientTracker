768a102a9cd4c93675de0bfcff5931436e6501d2
Ben Valkin <benvalkin@outlook.com>
2021-04-06 23:56:20 +0200

    Repaired project after merging all branches

--- app/src/main/java/com/acpc/mobilepatienttracker/DoctorRegistration.java
+++ app/src/main/java/com/acpc/mobilepatienttracker/DoctorRegistration.java
@@ -3,16 +3,10 @@ package com.acpc.mobilepatienttracker;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.content.Intent;
-import android.text.TextUtils;
-
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
-import android.widget.EditText;
-import android.os.Bundle;
 import android.app.DatePickerDialog;
-import android.content.Intent;
 import android.text.InputFilter;
 import android.widget.CheckBox;
 import android.widget.DatePicker;
