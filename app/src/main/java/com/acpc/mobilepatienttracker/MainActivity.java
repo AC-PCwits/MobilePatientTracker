fa21229022f7b74a07a0e23c1dbf338bbad4a254
Nkosinathi-byte <1614681@students.wits.ac.za>
2021-04-08 03:45:13 +0200

    second commit

--- app/src/main/java/com/acpc/mobilepatienttracker/MainActivity.java
+++ app/src/main/java/com/acpc/mobilepatienttracker/MainActivity.java
@@ -56,7 +56,7 @@ public class MainActivity extends AppCompatActivity {
         registration.setOnClickListener(new View.OnClickListener() { //what happens when you click the register button
             @Override
             public void onClick(View v) {
-                Intent start = new Intent(MainActivity.this,DoctorOrPatientt.class); //moving from main screen to reg screen when clicking register button on main screen
+                Intent start = new Intent(MainActivity.this,PRegistration.class); //moving from main screen to reg screen when clicking register button on main screen
                 startActivity(start);
 
             }
