cccb535c43a80e3087ea09938f0b308c076aaa1d
Clio <clionoel@gmail.com>
2021-04-22 13:55:40 +0200

    Consult Form UI

---
+++ app/src/main/java/com/acpc/mobilepatienttracker/DoctorConsultForm.java
@@ -0,0 +1,14 @@
+package com.acpc.mobilepatienttracker;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class DoctorConsultForm extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_doctor_consult_form);
+    }
+}
