a2948c0437b87c0d06ed809ddde79ad44b5bde14
2105746 <2105746@students.wits.ac.za>
2021-04-26 15:01:32 +0200

    Doctor Details is showing now

--- app/src/main/java/com/acpc/mobilepatienttracker/DHomePage.java
+++ app/src/main/java/com/acpc/mobilepatienttracker/DHomePage.java
@@ -3,6 +3,7 @@ package com.acpc.mobilepatienttracker;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.MenuItem;
--- app/src/main/java/com/acpc/mobilepatienttracker/DoctorDetails.java
+++ app/src/main/java/com/acpc/mobilepatienttracker/DoctorDetails.java
@@ -8,7 +8,16 @@ import android.widget.TextView;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.firestore.QuerySnapshot;
+
+import java.util.ArrayList;
 
 public class DoctorDetails extends AppCompatActivity {
 
@@ -34,6 +43,9 @@ public class DoctorDetails extends AppCompatActivity {
     private TextView uniText;
     private TextView specText;
 
+    private FirebaseFirestore database = FirebaseFirestore.getInstance();
+    private Doctor doc = new Doctor();
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -53,34 +65,36 @@ public class DoctorDetails extends AppCompatActivity {
 
         Bundle bundle = getIntent().getExtras();
 
-        if(bundle != null)
-        {
-            name = bundle.getString("DOC_NAME");
-            id = bundle.getString("DOC_ID");
-            dob = bundle.getString("DOC_DOB");
-            gender = bundle.getString("DOC_GENDER");
-            email = bundle.getString("DOC_EMAIL");
-            prac_length = bundle.getString("DOC_PRACYEARS");
-            prac_num = bundle.getString("DOC_PRACNUM");
-            uni = bundle.getString("DOC_UNI");
-            spec = bundle.getString("DOC_SPEC");
-            cellno = bundle.getString("DOC_CELL");
+//        if(bundle != null)
+//        {
+//            name = bundle.getString("DOC_NAME");
+//            id = bundle.getString("DOC_ID");
+//            dob = bundle.getString("DOC_DOB");
+//            gender = bundle.getString("DOC_GENDER");
+//            email = bundle.getString("DOC_EMAIL");
+//            prac_length = bundle.getString("DOC_PRACYEARS");
+//            prac_num = bundle.getString("DOC_PRACNUM");
+//            uni = bundle.getString("DOC_UNI");
+//            spec = bundle.getString("DOC_SPEC");
+//            cellno = bundle.getString("DOC_CELL");
+//
+//            nameText.setText(name);
+//            idText.setText(id);
+//            dobText.setText(dob);
+//            genderText.setText(gender);
+//            emailText.setText(email);
+//            pracLengthText.setText(prac_length);
+//            pracNumText.setText(prac_num);
+//            uniText.setText(uni);
+//            specText.setText(spec);
+//            cellText.setText(cellno);
+//        }
+//        else
+//        {
+//            nameText.setText("ERROR: NULL VALUE PASSED");
+//        }
 
-            nameText.setText(name);
-            idText.setText(id);
-            dobText.setText(dob);
-            genderText.setText(gender);
-            emailText.setText(email);
-            pracLengthText.setText(prac_length);
-            pracNumText.setText(prac_num);
-            uniText.setText(uni);
-            specText.setText(spec);
-            cellText.setText(cellno);
-        }
-        else
-        {
-            nameText.setText("ERROR: NULL VALUE PASSED");
-        }
+        getDocDet();
 
         BottomNavigationView bottomNavigationView = findViewById(R.id.d_nav_bar);
 
@@ -118,5 +132,74 @@ public class DoctorDetails extends AppCompatActivity {
 
 
 
+    }
+
+    public void getDocDet()
+    {
+        final FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+
+
+//        final String[] str = {""};
+//
+//        DatabaseReference reference = FirebaseDatabase.getInstance().getReference().child("Doctors");
+//        reference.addValueEventListener(new ValueEventListener() {
+//
+//            @Override
+//            public void onDataChange(@NonNull DataSnapshot snapshot) {
+//                for(DataSnapshot dataSnapshot : snapshot.getChildren())
+//                {
+//                    if(dataSnapshot.child("email").getValue().toString().equalsIgnoreCase(user.getEmail()))
+//                    {
+////                            str[0] = dataSnapshot.child("IDnum").getValue().toString();
+//                        testView.setText(dataSnapshot.child("IDnum").getValue().toString());
+//                    }
+//                }
+//
+//            }
+//
+//
+//            @Override
+//            public void onCancelled(@NonNull DatabaseError error) {
+//
+//            }
+//        });
+
+        database.collection("doctor-data").whereEqualTo("email", user.getEmail())
+                .get().addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+            @Override
+            public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                if(task.isSuccessful())
+                {
+                    ArrayList<Doctor> doctor1 = new ArrayList<>();
+
+                    for(QueryDocumentSnapshot doc : task.getResult())
+                    {
+                        doctor1.add(doc.toObject(Doctor.class));
+                    }
+
+                    String s = "";
+
+                    for(Doctor doctor2 : doctor1)
+                    {
+                        if(doctor2.email.equals(user.getEmail()))
+                        {
+                            doc = doctor2;
+                        }
+
+                    }
+
+                    nameText.setText(doc.fname + " " + doc.lname);
+                    idText.setText(doc.ID);
+                    dobText.setText(doc.dob);
+                    genderText.setText(doc.gender);
+                    emailText.setText(doc.email);
+                    pracLengthText.setText(String.valueOf(doc.p_length));
+                    pracNumText.setText(doc.p_no);
+                    uniText.setText(doc.uni_name);
+                    specText.setText(doc.doc_type);
+                    cellText.setText(doc.cell_no);
+                }
+            }
+        });
     }
 }
