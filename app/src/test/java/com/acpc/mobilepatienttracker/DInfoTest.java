20182b322208d064b7bc02d1f37fd5c1c2431c74
2105746 <2105746@students.wits.ac.za>
2021-05-13 14:47:31 +0200

    Fixed DInfoTest

--- app/src/test/java/com/acpc/mobilepatienttracker/DInfoTest.java
+++ app/src/test/java/com/acpc/mobilepatienttracker/DInfoTest.java
@@ -9,15 +9,17 @@ public class DInfoTest
     public String fname;
     public String sname;
     public String exp;
+    public String id;
 
     @Test
     public void testDInfoObject()
     {
-        DInfo dInfo = new DInfo(fname, sname, exp);
+        DInfo dInfo = new DInfo(fname, sname, exp, id);
 
         assertEquals(fname, dInfo.fname);
         assertEquals(sname, dInfo.sname);
         assertEquals(exp, dInfo.exp);
+        assertEquals(id, dInfo.pid);
         assertNotNull(new DInfo());
     }
 }
