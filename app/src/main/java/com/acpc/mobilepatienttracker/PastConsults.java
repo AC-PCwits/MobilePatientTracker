dfebaeaf35967aaf380c838f6473db1d983de0e4
1614681 <1614681@students.wits.ac.za>
2021-05-14 14:27:15 +0200

    past consult

---
+++ app/src/main/java/com/acpc/mobilepatienttracker/PastConsults.java
@@ -0,0 +1,14 @@
+package com.acpc.mobilepatienttracker;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class PastConsults extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_past_consults);
+    }
+}
