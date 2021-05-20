01b1db5642d7211dd5e9a4034336d55876c62c1c
2105746 <2105746@students.wits.ac.za>
2021-05-20 08:26:20 +0200

    Fixed Minor issues in DoctorConsultForm

--- app/src/main/java/com/acpc/mobilepatienttracker/PHomePage.java
+++ app/src/main/java/com/acpc/mobilepatienttracker/PHomePage.java
@@ -7,14 +7,35 @@ import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.AsyncListUtil;
 
+import com.github.jhonnyx2012.horizontalpicker.DatePickerListener;
+import com.github.jhonnyx2012.horizontalpicker.HorizontalPicker;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.firestore.QuerySnapshot;
+
+import org.joda.time.DateTime;
+
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Date;
 
 public class PHomePage extends Fragment {
 
@@ -26,6 +47,17 @@ public class PHomePage extends Fragment {
     // TODO: Rename and change types of parameters
     private String mParam1;
     private String mParam2;
+
+    private TextView bookingDate, bookingTime, bookingDoc, docID;
+
+    private ArrayList<AccOrRej> acceptRejects = new ArrayList<>();
+    private String docName = "John Smith";
+    private HorizontalPicker picker;
+
+    public interface DateCallBack
+    {
+        void onResponse(ArrayList<AccOrRej> list);
+    }
 
     public PHomePage() {
         // Required empty public constructor
@@ -65,7 +97,116 @@ public class PHomePage extends Fragment {
         super.onCreateView(inflater, container, savedInstanceState);
         View rootView = inflater.inflate(R.layout.activity_p_home_page, container, false);
 
+        bookingDate = rootView.findViewById(R.id.bookingDate);
+        bookingTime = rootView.findViewById(R.id.bookingTime);
+        bookingDoc = rootView.findViewById(R.id.bookingDoc);
+        docID = rootView.findViewById(R.id.docID);
+
+        picker = rootView.findViewById(R.id.datePicker);
+
+
+
+
+        getUserData(picker, new DateCallBack() {
+            @Override
+            public void onResponse(final ArrayList<AccOrRej> list) {
+
+                picker.setListener(new DatePickerListener() {
+                            @Override
+                            public void onDateSelected(DateTime dateSelected)
+                            {
+                                String [] date = dateSelected.toString().substring(0,10).split("-");
+                                String formdate = date[0] + "/" + date[1] + "/" + date[2];
+
+                                bookingDate.setText("Date: " + formdate);
+
+                                for(AccOrRej acceptReject : list)
+                                {
+                                    if(acceptReject.bookingdate.equals(formdate))
+                                    {
+                                        bookingDate.setText("Date: " + acceptReject.bookingdate);
+                                        bookingTime.setText("Time: " + acceptReject.time);
+                                        docID.setText("Doctor ID: " + acceptReject.doc_id);
+                                        bookingDoc.setText("Doctor: " + docName);
+                                        return;
+                                    }
+                                }
+
+                                bookingDate.setText("Date: ");
+                                bookingTime.setText("Time: ");
+                                docID.setText("Doctor ID: ");
+                                bookingDoc.setText("Doctor: ");
+                            }
+                        })
+                        .showTodayButton(true)
+                        .setOffset(3)
+                        .init();
+
+                SimpleDateFormat formatter = new SimpleDateFormat("yyyy/MM/dd");
+                Date date = new Date();
+
+                for(AccOrRej acceptReject : list)
+                {
+                    bookingDate.setText("Date: " + acceptReject.bookingdate);
+                    bookingTime.setText("Time: " + acceptReject.time);
+                    docID.setText("Doctor ID: " + acceptReject.doc_id);
+                    bookingDoc.setText("Doctor: " + docName);
+
+                    if(acceptReject.bookingdate.equals(formatter.format(date)))
+                    {
+                        bookingDate.setText("Date: " + acceptReject.bookingdate);
+                        bookingTime.setText("Time: " + acceptReject.time);
+                        docID.setText("Doctor ID: " + acceptReject.doc_id);
+                        bookingDoc.setText("Doctor: " + docName);
+                    }
+                }
+
+            }
+        });
+
         return rootView;
 
     }
+
+    public void getUserData(final HorizontalPicker picker, final DateCallBack callBack) {
+
+        final FirebaseFirestore database = FirebaseFirestore.getInstance();
+        final FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+
+        DatabaseReference reference = FirebaseDatabase.getInstance().getReference().child("Patient");
+        reference.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                for (DataSnapshot dataSnapshot : snapshot.getChildren()) {
+                    if (dataSnapshot.child("email").getValue().toString().equalsIgnoreCase(user.getEmail())) {
+                        final String ID = dataSnapshot.child("id").getValue().toString();
+
+                        database.collection("acc-rej-data").whereEqualTo("id", ID)
+                                .get().addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+                            @Override
+                            public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                                if (task.isSuccessful()) {
+
+                                    for (QueryDocumentSnapshot booking : task.getResult()) {
+                                        acceptRejects.add(booking.toObject(AccOrRej.class));
+                                    }
+
+                                    callBack.onResponse(acceptRejects);
+
+                                }
+                            }
+                        });
+                    }
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
+
+
+    }
+
 }
