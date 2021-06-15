3f2c7eddbb131139e0637e8c06ce7e6681a49c13
nkosinathi-byte <1614681@students.wits.ac.za>
2021-06-15 07:57:36 +0200

    UpdatedConForm

--- app/src/main/java/com/acpc/mobilepatienttracker/ConsultationDetails.java
+++ app/src/main/java/com/acpc/mobilepatienttracker/ConsultationDetails.java
@@ -7,7 +7,11 @@ import android.content.Intent;
 import android.graphics.Paint;
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.CompoundButton;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
@@ -17,6 +21,7 @@ import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.firebase.firestore.QuerySnapshot;
 
 import java.util.ArrayList;
+import java.util.Objects;
 
 public class ConsultationDetails extends AppCompatActivity {
 
@@ -35,13 +40,41 @@ public class ConsultationDetails extends AppCompatActivity {
     private TextView txtDate;
     private TextView txtConsultationDetailsLabel;
 
+
+    RadioButton radio0, radio1, radio2, radio3;
+    RadioGroup RadioGroup1;
+
+    String Acute,Chronic,Existing,Injury;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_consultation_details);
+        setContentView(R.layout.check);
 
-        txtConsultationDetailsLabel = findViewById(R.id.txtConsultationDetailsLabel);
-        txtConsultationDetailsLabel.setPaintFlags(txtConsultationDetailsLabel.getPaintFlags() | Paint.UNDERLINE_TEXT_FLAG);// to underline text
+        RadioGroup1 = (RadioGroup) findViewById(R.id.radioGroup4);
+
+        radio0 = (RadioButton) findViewById(R.id.radioButton);
+        radio1 = (RadioButton) findViewById(R.id.radioButton2);
+        radio2 = (RadioButton) findViewById(R.id.radioButton3);
+        radio3 = (RadioButton) findViewById(R.id.radioButton4);
+
+
+        Acute = "Acute";
+        Chronic = "Chronic";
+        Existing = "Existing";
+        Injury = "Injury";
+
+
+
+
+        //txtConsultationDetailsLabel = findViewById(R.id.txtConsultationDetailsLabel);
+        //txtConsultationDetailsLabel.setPaintFlags(txtConsultationDetailsLabel.getPaintFlags() | Paint.UNDERLINE_TEXT_FLAG);// to underline text
+
+        final LoadingDialog loadingDialog = new LoadingDialog(ConsultationDetails.this);
+
+        loadingDialog.startLoading();
+
+
         txtPFirstName = findViewById(R.id.txtPFirstname);
         txtPSurname = findViewById(R.id.txtPSurname);
         txtPatientID = findViewById(R.id.txtID);
@@ -66,71 +99,85 @@ public class ConsultationDetails extends AppCompatActivity {
             Log.d("CONSULTATION-DETAILS", doctorID);
             Log.d("CONSULTATION-DETAILS", dateTime);
 
-            GetPastConsult(patientID, doctorID, dateTime);
+            GetPastConsult(patientID, doctorID, dateTime,loadingDialog);
+
         }
         else
         {
             Log.w("CONSULTATION-DETAILS", "Bundle was null");
         }
+
+        //  loadingDialog.dismiss();
+
     }
 
-    public void GetPastConsult(final String patientID, final String doctorID, final String dateTime)
+    public void GetPastConsult(final String patientID, final String doctorID, final String dateTime, final LoadingDialog loadingDialog)
     {
         database.collection("doctor-data").whereEqualTo("p_no", doctorID)
                 .get().addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
             @Override
             public void onComplete(@NonNull Task<QuerySnapshot> task) {
-                if(task.isSuccessful())
-                {
+                if (task.isSuccessful()) {
                     final Doctor doctor = task.getResult().toObjects(Doctor.class).get(0);
 
                     database.collection("patient-data").whereEqualTo("idno", patientID)
                             .get().addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
-                             @Override
-                             public void onComplete(@NonNull Task<QuerySnapshot> task) {
-                                 if (task.isSuccessful()) {
 
-                                     final Patient patient = task.getResult().toObjects(Patient.class).get(0);
+                        @Override
+                        public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                            if (task.isSuccessful()) {
 
-                                     database.collection("consultation-data").whereEqualTo("pdoctorID", doctorID).whereEqualTo("ppatientID", patientID).whereEqualTo("pdate", dateTime)
-                                             .get().addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
-                                         @Override
-                                         public void onComplete(@NonNull Task<QuerySnapshot> task) {
-                                             if(task.isSuccessful())
-                                             {
-                                                 Consultation consultation = task.getResult().toObjects(Consultation.class).get(0);
+                                final Patient patient = task.getResult().toObjects(Patient.class).get(0);
 
-                                                 txtPFirstName.setText(patient.fname);
-                                                 txtPSurname.setText(patient.fsurname);
-                                                 txtPatientID.setText(patient.idno);
-                                                 txtPcell.setText(patient.cellno);
-                                                 txtDFirstName.setText(doctor.fname);
-                                                 txtDSurname.setText(doctor.lname);
-                                                 txtPracticeID.setText(doctor.p_no);
-                                                 txtCaseInfo.setText(consultation.pcase);
-                                                 txtSymptoms.setText(consultation.psymptoms);
-                                                 txtDiagnosis.setText(consultation.pdiagnosis);
-                                                 txtDate.setText(consultation.pdate);
-                                             }
-                                             else
-                                             {
-                                                 Log.w("CONSULTATION-DETAILS", "Could not get past consults: ", task.getException());
-                                             }
-                                         }
-                                     });
-                                 }
-                                 else
-                                 {
-                                     Log.w("CONSULTATION-DETAILS", "Could not get patient info: ", task.getException());
-                                 }
-                             }
-                         });
-                }
-                else
-                {
-                    Log.w("CONSULTATION-DETAILS", "Could not get doctor info: ", task.getException());
+                                database.collection("consultation-data").whereEqualTo("pdoctorID", doctorID).whereEqualTo("ppatientID", patientID).whereEqualTo("pdate", dateTime)
+                                        .get().addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+                                    @Override
+                                    public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                                        if (task.isSuccessful()) {
+                                            Consultation consultation = task.getResult().toObjects(Consultation.class).get(0);
+
+                                            txtPFirstName.setText(patient.fname);
+                                            txtPSurname.setText(patient.fsurname);
+                                            txtPatientID.setText(patient.idno);
+                                            txtPcell.setText(patient.cellno);
+                                            txtDFirstName.setText(doctor.fname);
+                                            //                         txtDSurname.setText(doctor.lname);
+                                            txtPracticeID.setText(doctor.p_no);
+                                            //txtCaseInfo.setText(consultation.pcase);
+                                            txtSymptoms.setText(consultation.psymptoms);
+                                            txtDiagnosis.setText(consultation.pcase);
+                                            txtDate.setText(consultation.pdate);
+
+
+                                            if (Acute.equals(consultation.pdiagnosis)) {
+                                                radio0.setChecked(true);
+                                            } else if (Chronic.equals(consultation.pdiagnosis)) {
+                                                radio1.setChecked(true);
+
+                                            } else if (Existing.equals(consultation.pdiagnosis)) {
+                                                radio2.setChecked(true);
+
+                                            } else if (Injury.equals(consultation.pdiagnosis)) {
+                                                radio3.setChecked(true);
+
+                                            }
+                                            loadingDialog.dismiss();
+
+
+                                        } else {
+                                            Log.w("CONSULTATION-DETAILS", "Could not get past consults: ", task.getException());
+                                        }
+                                    }
+                                });
+                            } else {
+                                Log.w("CONSULTATION-DETAILS", "Could not get patient info: ", task.getException());
+                            }
+                        }
+                    });
                 }
             }
+
         });
+
     }
 }
